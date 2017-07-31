5c8e60b027b19e48b82aa2397b79e7afcc8a1121
Franklin Dias <franklindias99@gmail.com>
2017-07-30 21:19:35 -0300

    Implementa tela de login e corrige alguns links

--- eajforms/urls.py
+++ eajforms/urls.py
@@ -5,8 +5,8 @@ from eajforms.core import views as core_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', core_views.home, name='home'),
-    url(r'^accounts/login/$', login, name='login'),
+    url(r'^$', core_views.dashboard, name='home'),
+    url(r'^accounts/login/$', login, name='login', kwargs={'redirect_authenticated_user': True}),
     url(r'^accounts/logout/$', logout, name='logout', kwargs={'next_page': '/'}),
     url(r'^dashboard/$', core_views.dashboard, name='dashboard'),
     url(r'', include('eajforms.forms.urls')),
